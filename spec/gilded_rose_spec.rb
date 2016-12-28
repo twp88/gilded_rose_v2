028c25f0fe25ec2524df50fd8b6d8050286b6ec8
Thomas William Page <thomaswilliampage@Thomass-MacBook-Pro.local>
2016-12-28 22:39:43 +0000

    slight improvement to gilded_rose_spec.rb

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,14 @@
 
 describe GildedRose do
 
+  before(:each) do
+    @items = [Item.new("foo", 0, 0)]
+  end
+
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      GildedRose.new(@items).update_quality()
+      expect(@items[0].name).to eq "foo"
     end
   end
 
