ffd299c913b2df73845092ff3edb4bc02bc1cd0e
Thomas William Page <tom.w.page@gmail.com>
2017-01-12 16:23:18 +0100

    refactoring of methods

--- lib/methods.rb
+++ lib/methods.rb
@@ -9,8 +9,8 @@ class Methods
   def aged_brie(item)
     @item = item
     @name = 'Aged Brie'
-    @item.sell_in -= 1
-    @item.quality += 1 if @item.quality < 50
+    lower_sell_in_by_one(@item)
+    alter_aged_brie_quality(@item)
   end
 
   def sulfuras(item)
@@ -21,19 +21,37 @@ class Methods
   def backstage(item)
     @item = item
     @name = 'Backstage passes to a TAFKAL80ETC concert'
-    @item.quality += 3 if @item.sell_in <= 5 && @item.quality < 50
-    @item.quality += 2 if @item.sell_in <= 11 && @item.sell_in >= 6 && @item.quality < 50
-    @item.quality += 1 if @item.sell_in > 11 && @item.quality < 50
-    @item.quality -= @item.quality if @item.sell_in <= 0
-    @item.sell_in -= 1
+    alter_backstage_quality(@item)
+    lower_sell_in_by_one(@item)
   end
 
   def normal(item)
     @item = item
     @name = 'Normal'
-    @item.quality -= 1 if @item.quality > 0
-    @item.quality -= 2 if @item.quality <= 0
-    @item.sell_in -= 1
+    alter_normal_quality(@item)
+    lower_sell_in_by_one(@item)
+  end
+
+private
+
+  def alter_backstage_quality(item)
+    item.quality += 3 if item.sell_in <= 5 && item.quality < 50
+    item.quality += 2 if item.sell_in <= 11 && item.sell_in >= 6 && item.quality < 50
+    item.quality += 1 if item.sell_in > 11 && item.quality < 50
+    item.quality -= item.quality if item.sell_in <= 0
+  end
+
+  def alter_normal_quality(item)
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 2 if item.quality <= 0
+  end
+
+  def lower_sell_in_by_one(item)
+    item.sell_in -= 1
+  end
+
+  def alter_aged_brie_quality(item)
+    item.quality += 1 if item.quality < 50
   end
 
 
