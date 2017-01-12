498f87670d806b92e91a38e0b565a2eb75dd25c7
Thomas William Page <tom.w.page@gmail.com>
2017-01-12 17:29:39 +0100

    yet more refactoring

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,15 +2,15 @@ class GildedRose
 
   attr_accessor :items, :quality, :sell_in, :item
 
-
   def update_quality(item)
-     if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    case item.name
+    when 'Backstage passes to a TAFKAL80ETC concert'
        Methods.new.backstage(item)
-     elsif item.name == 'Aged Brie'
+     when 'Aged Brie'
        Methods.new.aged_brie(item)
-     elsif item.name == 'Sulfuras, Hand of Ragnaros'
+     when 'Sulfuras, Hand of Ragnaros'
        Methods.new.sulfuras(item)
-     elsif item.name == 'Conjured'
+     when 'Conjured'
        Methods.new.conjured(item)
      else
        Methods.new.normal(item)
