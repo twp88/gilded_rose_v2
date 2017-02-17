9412478062fb64789ac7c6efca6f0edee0060ce0
Thomas William Page <tom.w.page@gmail.com>
2017-02-17 17:15:49 +0100

    tests updated to latest refactor

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -5,7 +5,6 @@
    before(:each) do
      @items = Item.new("foo", 0, 0)
      @gilded_rose = GildedRose.new
-     @methods = Methods.new
    end
 
    context 'more specific tests for #backstage' do
@@ -13,35 +12,35 @@
      describe '#backstage' do
        it 'It expects item.sell_in to reduce by 1 ' do
           @backstage = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40)
-          expect { @methods.lower_sell_in_by_one(@backstage) }.to change{ @backstage.sell_in }.by(-1)
+          expect { @backstage.lower_sell_in_by_one }.to change{ @backstage.sell_in }.by(-1)
        end
      end
 
      describe '#backstage' do
        it 'It expects the quality to change by 1 if sell_in is > 11' do
          @backstage1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 40)
-         expect { @methods.alter_backstage_quality(@backstage1) }.to change{@backstage1.quality}.by(1)
+         expect { @backstage1.alter_backstage_quality }.to change{@backstage1.quality}.by(1)
        end
      end
 
      describe '#backstage' do
        it 'It expects the quality to change by 2 if sell_in is < 11' do
          @backstage2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40)
-         expect { @methods.alter_backstage_quality(@backstage2) }.to change{@backstage2.quality}.by(2)
+         expect { @backstage2.alter_backstage_quality }.to change{@backstage2.quality}.by(2)
        end
      end
 
      describe '#backstage' do
        it 'It expects the quality to change by 3 if sell_in is < 6' do
          @backstage3 = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 40)
-         expect { @methods.alter_backstage_quality(@backstage3) }.to change{@backstage3.quality}.by(3)
+         expect { @backstage3.alter_backstage_quality }.to change{@backstage3.quality}.by(3)
        end
      end
 
      describe '#backstage' do
        it 'It expects the quality go to 0 if sell_in == 0' do
          @backstage3 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)
-         expect { @methods.alter_backstage_quality(@backstage3) }.to change{@backstage3.quality}.by(-@backstage3.quality)
+         expect { @backstage3.alter_backstage_quality }.to change{@backstage3.quality}.by(-@backstage3.quality)
        end
      end
 
@@ -52,21 +51,21 @@
      describe '#normal' do
        it 'It expects item.sell_in to reduce by 1 ' do
           @normal = Item.new("Normal", 10, 40)
-          expect { @methods.lower_sell_in_by_one(@normal) }.to change{ @normal.sell_in }.by(-1)
+          expect { @normal.lower_sell_in_by_one }.to change{ @normal.sell_in }.by(-1)
        end
      end
 
      describe '#normal' do
        it 'It expects item.sell_in to reduce by 2 if sell_in < 0 ' do
           @normal = Item.new("Normal", 10, 0)
-          expect { @methods.alter_normal_quality(@normal) }.to change{ @normal.quality }.by(-2)
+          expect { @normal.alter_normal_quality }.to change{ @normal.quality }.by(-2)
        end
      end
 
      describe '#normal' do
        it 'It expects quality to reduce by 1 if item.quality > 0' do
           @normal = Item.new("Normal", 10, 40)
-          expect { @methods.alter_normal_quality(@normal) }.to change{ @normal.quality }.by(-1)
+          expect { @normal.alter_normal_quality }.to change{ @normal.quality }.by(-1)
        end
      end
    end
@@ -76,14 +75,14 @@
      describe '#aged_brie' do
        it 'It expects item.sell_in to reduce by 1 ' do
           @aged_brie = Item.new("Aged Brie", 10, 40)
-          expect { @methods.lower_sell_in_by_one(@aged_brie) }.to change{ @aged_brie.sell_in }.by(-1)
+          expect { @aged_brie.lower_sell_in_by_one }.to change{ @aged_brie.sell_in }.by(-1)
        end
      end
 
      describe '#aged_brie' do
        it 'It expects item.quality to increase by 1 if quality < 50 ' do
           @aged_brie = Item.new("Aged Brie", 10, 40)
-          expect { @methods.alter_aged_brie_quality(@aged_brie) }.to change{ @aged_brie.quality }.by(+1)
+          expect { @aged_brie.alter_aged_brie_quality }.to change{ @aged_brie.quality }.by(+1)
        end
      end
    end
@@ -92,14 +91,14 @@
      describe '#conjured' do
        it 'It expects quality to reduce by 2 if item.quality > 0' do
           @conjured = Item.new("Conjured", 10, 40)
-          expect { @methods.alter_conjured_quality(@conjured) }.to change{ @conjured.quality }.by(-2)
+          expect { @conjured.alter_conjured_quality }.to change{ @conjured.quality }.by(-2)
        end
      end
 
      describe '#aged_brie' do
        it 'It expects item.sell_in to reduce by 1 ' do
           @conjured = Item.new("Conjured", 10, 40)
-          expect { @methods.lower_sell_in_by_one(@conjured) }.to change{ @conjured.sell_in }.by(-1)
+          expect { @conjured.lower_sell_in_by_one }.to change{ @conjured.sell_in }.by(-1)
        end
      end
    end
